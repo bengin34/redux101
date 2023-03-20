974ea30a499b65b77ecced506f7dd7ce80fff47d
bengin34 <becaglar3434@gmail.com>
2023-03-20 19:50:16 +0300

    v5

--- src/features/user.js
+++ src/features/user.js
@@ -4,10 +4,13 @@ import { createSlice } from '@reduxjs/toolkit'
 
 const userSlice = createSlice({
     name: "user",
-    initialState: { value:{name: "", age: 0, email: "" }},
+    initialState: { value:{name: "Pedro", age: 0, email: "email@gmail.com" }},
     reducers: {
         login: (state, action) => {
             state.value = action.payload
         }
     }
 })
+
+export const {logim} = userSlice.actions
+export default userSlice.reducer;
