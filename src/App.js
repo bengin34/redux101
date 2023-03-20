75386a20d99bb029492416f665695d51e5fa8af0
bengin34 <becaglar3434@gmail.com>
2023-03-20 17:55:04 +0300

    v4

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { increment } from "./actions";
-import { decrement } from "./actions";
+import Profile from "./components/Profile";
+import Login from "./components/Login";
+
+
 function App() {
-  const counter = useSelector((state) => state.counter);
-  const isLogged = useSelector((state) => state.isLogged);
-const dispatch = useDispatch()
+// 
   return (
     <div className="App">
-      <h1>Counter {counter}</h1>
-      <button onClick={()=> dispatch(increment(5))} >+</button>
-      <button onClick={()=> dispatch(decrement())}>-</button>
-      {isLogged ? <h3>Valuable information I should not see</h3> : null}
+      <Profile />
+      <Login />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { createStore } from 'redux';
-import allReducer from './reducers'
-import { Provider } from 'react-redux';
-
-
-const store = createStore(allReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
-
+import {configureStore} from '@reduxjs/toolkit'
+import {Provider} from 'react-redux'
+const store = configureStore({
+  reducer: {},
+  
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
